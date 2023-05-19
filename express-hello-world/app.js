48f1b6fbf4bf20ad6da8b8d5da0529139515519e
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-05-20 01:22:08 +0200

    pages

--- express-hello-world/app.js
+++ express-hello-world/app.js
@@ -1,9 +1,6 @@
 const express = require("express");
-
-// We create our own server named app
-// Express server will be handling requests and responses
-const express = require("express");
 const app = express();
+const port = 3000;
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 
@@ -11,6 +8,21 @@ app.use("/static", express.static(path.join(__dirname, "public")));
 app.get("/home", (request, response, next) => {
   console.log(request);
   response.send("<h1>Welcome Ironhacker. :)</h1>");
+});
+
+app.get("/about", (request, response, next) => {
+  console.log(request);
+  response.send("<h1>About Ironhacker.</h1>");
+});
+
+app.get("/Work", (request, response, next) => {
+  console.log(request);
+  response.send("<h1>About Ironhacker.</h1>");
+});
+
+app.get("/Photo-Gallery", (request, response, next) => {
+  console.log(request);
+  response.send("<h1>About Ironhacker.</h1>");
 });
 
 app.listen(3000);
