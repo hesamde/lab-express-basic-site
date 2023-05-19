14b769f01f913ac6a7126615c7377ee254d9fe37
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-05-20 01:30:23 +0200

    pages

--- express-hello-world/app.js
+++ express-hello-world/app.js
@@ -5,24 +5,20 @@ const port = 3000;
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 // our first Route
-app.get("/home", (request, response, next) => {
-  console.log(request);
-  response.send("<h1>Welcome Ironhacker. :)</h1>");
+app.get("/home", (request, response) => {
+  res.send("<h1> Home PAge</h1>");
 });
 
-app.get("/about", (request, response, next) => {
-  console.log(request);
-  response.send("<h1>About Ironhacker.</h1>");
+app.get("/about", (request, response) => {
+  res.send("<h1>About Me Page .</h1>");
 });
 
-app.get("/Work", (request, response, next) => {
-  console.log(request);
-  response.send("<h1>About Ironhacker.</h1>");
+app.get("/Work", (request, response) => {
+  res.send("<h1>My Work  Page.</h1>");
 });
 
-app.get("/Photo-Gallery", (request, response, next) => {
-  console.log(request);
-  response.send("<h1>About Ironhacker.</h1>");
+app.get("/Photo-Gallery", (req, res) => {
+  res.send("<h1>Photo Page.</h1>");
 });
 
 app.listen(3000);
