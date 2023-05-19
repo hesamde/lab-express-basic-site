6e3038fde8a573ddc0a5699cb09b65de63434c03
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-05-20 01:59:40 +0200

    pages

--- express-hello-world/app.js
+++ express-hello-world/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/", express.static(path.join(__dirname, "public")));
 
 // our first Route
 app.get("/home", (req, res) => {
